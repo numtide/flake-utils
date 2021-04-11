4ce446623b84d91005620bf3fb879bdb0a0a8fe7
David Arnold <dar@xoe.solutions>
2021-04-11 11:51:19 -0500

    imp: filterPackages

---
+++ filterPackages.nix
@@ -0,0 +1,27 @@
+system: packages:
+let
+  # Adopted from nixpkgs.lib
+  inherit (builtins) listToAttrs concatMap attrName;
+  nameValuePair = name: value: { inherit name value; };
+  filterAttrs = pred: set:
+    listToAttrs (
+      concatMap (name:
+        let v = set.${name}; in
+          if pred name v then [(nameValuePair name v)] else []
+      )
+      (attrNames set)
+    );
+
+  # Everything that nix flake check requires for the packages output
+  sieve = (n: v: with v;
+    let
+      inherit (builtins) isAttrs;
+      isDerivation = x: isAttrs x && x ? type && x.type == "derivation";
+      platforms = meta.hydraPlatforms or meta.platforms or [ ];
+    in
+      # check for isDerivation, so this is independently useful of
+      # flattenTree, which also does filter on derviations
+      isDerivation v && !meta.broken && builtins.elem system platforms
+  );
+in
+filterAttrs sieve packages
