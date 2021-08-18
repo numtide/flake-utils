c5d161cc0af116a2e17f54316f0bf43f0819785c
Jörg Thalheim <Mic92@users.noreply.github.com>
2021-08-18 12:06:51 +0100

    add aarch64-darwin as default system (#40)

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ let
   # The list of systems supported by nixpkgs and hydra
   defaultSystems = [
     "aarch64-linux"
+    "aarch64-darwin"
     "i686-linux"
     "x86_64-darwin"
     "x86_64-linux"
