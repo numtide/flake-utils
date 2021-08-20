acfb1951f051283fe3fed23045d09144e0ab5dd2
zimbatm <zimbatm@zimbatm.com>
2021-08-20 17:28:30 +0200

    Revert "add aarch64-darwin as default system (#40)"
    This reverts commit c5d161cc0af116a2e17f54316f0bf43f0819785c.
    
    This won't work if nixpkgs isn't also up to speed with aarch64-darwin.

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@ let
   # The list of systems supported by nixpkgs and hydra
   defaultSystems = [
     "aarch64-linux"
-    "aarch64-darwin"
     "i686-linux"
     "x86_64-darwin"
     "x86_64-linux"
