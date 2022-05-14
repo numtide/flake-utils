dd27e453304b30be954df164a0b7aaf4b02305b4
Jörg Thalheim <joerg@thalheim.io>
2022-05-14 21:22:05 +0200

    expose examples as templates

--- flake.nix
+++ flake.nix
@@ -2,5 +2,20 @@
   description = "Pure Nix flake utility functions";
   outputs = { self }: {
     lib = import ./.;
+    templates = {
+      simple-flake = {
+        path = ./examples/simple-flake;
+        description = "A flake using flake-utils.lib.simpleFlake";
+      };
+      each-system = {
+        path = ./examples/each-system;
+        description = "A flake using flake-utils.lib.eachDefaultSystem";
+      };
+      check-utils = {
+        path = ./examples/check-utils;
+        description = "A flake with tests";
+      };
+    };
+    defaultTemplate = self.templates.each-system;
   };
 }
