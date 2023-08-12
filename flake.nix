0c8f2a323469cb6ddda6d19dc64004564db65f44
Gerg-L <GregLeyda@proton.me>
2023-08-12 18:41:06 -0400

    made better

--- flake.nix
+++ flake.nix
@@ -3,24 +3,30 @@
 
   # Externally extensible flake systems. See <https://github.com/nix-systems/nix-systems>.
   inputs.systems.url = "github:nix-systems/default";
+  # :D
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = { self, systems }: {
-    lib = import ./lib.nix {
-      defaultSystems = import systems;
-    };
-    templates = {
-      default = self.templates.each-system;
-      simple-flake = {
-        path = ./examples/simple-flake;
-        description = "A flake using flake-utils.lib.simpleFlake";
+  outputs = inputs @ { flake-parts, self, ...}:
+  flake-parts.lib.mkFlake { inherit inputs; }
+  {
+    flake = {
+      lib = import ./lib.nix {
+        defaultSystems = import inputs.systems;
       };
-      each-system = {
-        path = ./examples/each-system;
-        description = "A flake using flake-utils.lib.eachDefaultSystem";
-      };
-      check-utils = {
-        path = ./examples/check-utils;
-        description = "A flake with tests";
+      templates = {
+        default = self.templates.each-system;
+        simple-flake = {
+          path = ./examples/simple-flake;
+          description = "A flake using flake-utils.lib.simpleFlake";
+        };
+        each-system = {
+          path = ./examples/each-system;
+          description = "A flake using flake-utils.lib.eachDefaultSystem";
+        };
+        check-utils = {
+          path = ./examples/check-utils;
+          description = "A flake with tests";
+        };
       };
     };
   };
